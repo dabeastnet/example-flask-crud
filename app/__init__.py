c813800e77b738b57f37902fecce176d883738b5
Dieter Beckers <dabeastmods@gmail.com>
2025-10-26 12:21:47 +0100

    change entry

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,7 @@ from flask_migrate import Migrate
 from werkzeug.middleware.proxy_fix import ProxyFix
 import logging, sys
 
-# ensures logs go to stdout/stderr (works well with CloudWatch):
-if not app.debug:
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-
 app = Flask(__name__)
-# Trust a single reverse proxy (the ALB). If you add Nginx in front, set x_* accordingly.
-# Why: Without this, URLs, scheme (https), and client IPs can be wrong when sitting behind a proxy / ALB.
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 app.config.from_object(Config)
 #update configuration to enforce HTTPS cookies:
@@ -23,10 +14,17 @@ app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
     PREFERRED_URL_SCHEME='https'
 )
+# Trust a single reverse proxy (the ALB). If you add Nginx in front, set x_* accordingly.
+# Why: Without this, URLs, scheme (https), and client IPs can be wrong when sitting behind a proxy / ALB.
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
+# ensures logs go to stdout/stderr (works well with CloudWatch):
+if not app.debug:
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
 
 from app import routes, models
